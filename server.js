6dc12f8d3039fcf5ff762762624097fcefc5923a
Abhay-wdev <abhaychorsiya.dev@gmail.com>
2025-11-10 17:47:54 +0530

    ok

--- server.js
+++ server.js
@@ -22,6 +22,11 @@ import heroImageRoutes from "./routes/heroImageRoutes.js";
 import blogRoutes from "./routes/blogRoutes.js";
 import companyRoutes from "./routes/companyRoutes.js";
 import contactRoutes from "./routes/contactRoutes.js";
+import videoProductRoutes from "./routes/videoProductRoutes.js";
+import category from "./routes/category.js";
+import testimonialsRouter from "./routes/testimonialsRoutes.js";
+import distributorRoutes from "./routes/distributorRoutes.js";
+import subscriberRoutes from "./routes/subscriberRoutes.js"
 dotenv.config();
 
 const app = express();
@@ -53,7 +58,11 @@ app.use("/api/auth", auth);
 app.use("/api/blogs", blogRoutes);
 app.use("/api/company", companyRoutes);
 app.use("/api/contact", contactRoutes);
-
+app.use("/api/videoproducts", videoProductRoutes);
+app.use("/api/categoryCards", category);
+app.use("/api/testimonials", testimonialsRouter);
+app.use("/api/distributors", distributorRoutes);
+app.use("/api/subscribers", subscriberRoutes);
 app.get("/", (req, res) => res.send("API is running"));
 
 const PORT = process.env.PORT || 5000;
